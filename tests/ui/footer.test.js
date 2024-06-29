9d90b7ad3028a58152671a88d2dad0238115ea4e
Stilyan <117260079+stilyan122@users.noreply.github.com>
2024-06-29 11:18:49 +0300

    Fixed failing UI Tests

--- tests/ui/footer.test.js
+++ tests/ui/footer.test.js
@@ -4,6 +4,6 @@ test('Check footer', async ({ page }) => {
     await page.goto('http://localhost:8090');  
     const footer = await page.$('footer');
     const text = await footer.textContent();
-    expect(text).toContain('© 2023 - Software Engineering and DevOps exam preparation.');
+    expect(text).toContain('© 2024 - Software Engineering and DevOps regular exam');
   });
   
